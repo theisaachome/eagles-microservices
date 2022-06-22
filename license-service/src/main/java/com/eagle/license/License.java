979645f3a414916fc1032ef06ee67e3f92452d40
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-22 19:29:35 +0630

    project setup

---
+++ license-service/src/main/java/com/eagle/license/License.java
@@ -0,0 +1,9 @@
+package com.eagle.license;
+
+import lombok.Data;
+
+@Data
+public class License {
+
+	private Integer id;
+}
---
+++ license-service/src/main/java/com/eagle/license/LicenseApplication.java
@@ -0,0 +1,11 @@
+package com.eagle.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseApplication.class,args);
+	}
+}
